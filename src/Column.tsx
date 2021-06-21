86bcd43cad9ad02ce87074c885ffb74e75d03662
Kris Renaldi <krenaldi@comcast.net>
2021-06-21 01:23:37 -0400

    Added Context provider for state management

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import { ColumnContainer, ColumnTitle } from './styles'
 import { AddNewItem } from './AddNewItem'
+import { useAppState } from './state/AppStateContext'
+import { Card } from './Card'
 
-type ColumnProps = React.PropsWithChildren<{
+type ColumnProps = {
     text: string
-}>
+    id: string
+}
 
-export const Column: React.FC<ColumnProps> = ({ text, children }) => {
+export const Column = ({ text, id }: ColumnProps) => {
+    const { getTasksByListId } = useAppState()
+
+    const tasks = getTasksByListId(id)
+
     return (
         <ColumnContainer>
             <ColumnTitle>{text}</ColumnTitle>
-            {children}
+            {tasks.map(task => (
+                <Card text={task.text} key={task.id} id={task.id} />
+            ))}
             <AddNewItem
                 toggleButtonText="+ Add another task"
                 onAdd={console.log}
