935f1559862381191bc69e6c7862f2bd834f5ac9
Kris Renaldi <krenaldi@comcast.net>
2021-06-20 20:43:37 -0400

    Created styled divs for column and card layout

--- src/styles.ts
+++ src/styles.ts
@@ -9,3 +9,28 @@ export const AppContainer = styled.div`
     padding: 20px;
     background-color: #3179ba;
 `
+
+export const ColumnContainer = styled.div`
+    background-color: #ebecf0;
+    width: 300px;
+    min-height: 40px;
+    margin-right: 20px;
+    border-radius: 3px;
+    padding: 8px;
+    flex-grow: 0;
+`
+
+export const ColumnTitle = styled.div`
+    padding: 6px 16px 12px;
+    font-weight: bold;
+`
+
+export const CardContainer = styled.div`
+    background-colo: #fff;
+    cursor: pointer;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem 1rem;
+    max-width: 300px;
+    border-radius: 3px;
+    box-shadow: #091e4240 0px 1px 0px 0px;
+`
