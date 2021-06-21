fc0f0f7984c1ae9ff19a97433dd9c12bf5c68127
Kris Renaldi <krenaldi@comcast.net>
2021-06-20 20:27:03 -0400

    Setup files and folder structure

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    background-color: #3179ba;
+`
