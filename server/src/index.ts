ec21eee639f257ed4c59798e50d5482838d65c3b
Kris Renaldi <krenaldi@comcast.net>
2023-12-29 17:50:58 -0500

    Added default route to server index.ts file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,31 @@
-import express from "express"
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import path from "path";
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-let lists:any[] = [];
+let lists: any[] = [];
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 app.post("/save", (req, res) => {
-  lists = req.body.lists
-  return res.json({ success: true })
-})
+  lists = req.body.lists;
+  return res.json({ success: true });
+});
 
-app.get("/load", (req, res) => res.json({ lists }))
+app.get("/load", (req, res) => res.json({ lists }));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.listen(port, () =>
   console.log(`Kanban backend running on http://localhost:${port}!`)
-)
+);
